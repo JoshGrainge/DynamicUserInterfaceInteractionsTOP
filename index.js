c8e83018ca043e2ebbfa387485219bac145658f3
Josh Grainge <josh.grainge@hotmail.com>
2023-02-05 02:30:53 -0500

    Made simple collapsing header

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ dropdowns.forEach((dropdown) => {
   const menu = dropdown.querySelector(".menu");
 
   dropdownButton.addEventListener("mouseenter", () => {
-    console.log("moused over");
     menu.classList.add("show");
   });
 
   dropdown.addEventListener("mouseleave", () => {
     menu.classList.remove("show");
   });
+});
 
-  //dropdownButton.
+const more = document.querySelector(".more");
+const header = document.querySelector("header");
+
+more.addEventListener("mouseenter", () => {
+  header.classList.add("extend");
+});
+
+header.addEventListener("mouseleave", () => {
+  header.classList.remove("extend");
 });
