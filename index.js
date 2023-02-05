7768840b41c6769fd9f9ee8735a860ca7c6a7595
Josh Grainge <josh.grainge@hotmail.com>
2023-02-05 05:47:49 -0500

    Made simple slideshow

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 1st project
 const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach((dropdown) => {
@@ -13,6 +14,8 @@ dropdowns.forEach((dropdown) => {
   });
 });
 
+// 2nd project
+
 const more = document.querySelector(".more");
 const header = document.querySelector("header");
 
@@ -23,3 +26,35 @@ more.addEventListener("mouseenter", () => {
 header.addEventListener("mouseleave", () => {
   header.classList.remove("extend");
 });
+
+// 3rd project
+const images = document.querySelectorAll(".slideshow-container>img");
+const navButtons = document.querySelectorAll(".button-container > button");
+let currentImage = 0;
+
+navButtons.forEach((navButton) => {
+  navButton.addEventListener("click", (e) => {
+    // Stop autoscroll when player input starts
+    clearInterval(autoScroll);
+    gotoSpecificSlide(parseInt(e.target.id));
+  });
+});
+
+let autoScroll = window.setInterval(gotoNextSlide, 5000);
+
+function gotoNextSlide() {
+  images[currentImage].classList.remove("current");
+  navButtons[currentImage].classList.remove("current");
+  currentImage++;
+  if (currentImage >= images.length) currentImage = 0;
+  images[currentImage].classList.add("current");
+  navButtons[currentImage].classList.add("current");
+}
+
+function gotoSpecificSlide(index) {
+  images[currentImage].classList.remove("current");
+  navButtons[currentImage].classList.remove("current");
+  currentImage = index;
+  images[currentImage].classList.add("current");
+  navButtons[currentImage].classList.add("current");
+}
